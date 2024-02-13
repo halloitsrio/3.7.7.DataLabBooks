d81964c2baabd6f54cb9a001db80ddd2e8845574
halloitsrio <kanwar7970@mydusd.org>
2024-02-13 11:48:41 -0800

    trying to access kaggle dataeset

--- Temp.java
+++ Temp.java
@@ -2,10 +2,9 @@ import core.data.*;
 
 public class Temp {
     public static void main(String[] args) {
-        DataSource ds = DataSource.connect("http://weather.gov/xml/current_obs/KATL.xml");
+        DataSource ds = DataSource.connect("https://www.kaggle.com/datasets/shivamb/netflix-shows");
         ds.load();
-        float temp = ds.fetchFloat("temp_f");
-        System.out.println("Temperature: " + temp);
+        ds.printUsageString();
         
     }
 }
